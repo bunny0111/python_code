77dafa69e88bd3580f64ca82a1a5a4e244cad562
Shubham Raj <107382989+bunny0111@users.noreply.github.com>
2024-06-21 08:10:53 +0000

    Code on June 21

--- Pattern/5 Pyramid.py
+++ Pattern/5 Pyramid.py
@@ -3,3 +3,11 @@ Pyramid Shape
 
 '''
 
+def pyramid(rows):
+    for i in range(1, rows+1):
+        for j in range(rows - i):
+            print(' ', end='')
+        for j in range(2*i-1):
+            print('*', end='')
+        print()
+pyramid(5)
